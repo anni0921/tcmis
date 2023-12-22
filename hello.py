5d806cb8ad315c72104bb45397128206ea2ff73d
anni0921 <7002160@qq.com>
2023-12-22 21:47:31 +0800

    make it better

---
+++ hello.py
@@ -0,0 +1,6 @@
+x=int(input("请输入一个正整数："))
+if(x<=0):
+	print("您输入的值小于等于0")
+else:
+	for i in range(x):
+		print(i)
---
+++ mis.py
@@ -0,0 +1,22 @@
+def mis2a():
+	print("资管二A")
+
+def mis2b():
+	print("资管二B")
+
+import mis
+
+mis.mis2a()
+
+mis.mis2b()
+
+import mis as m
+
+m.mis2a()
+
+m.mis2b()
+from mis import mis2a,mis2b
+
+mis2a()
+
+mis2b()
